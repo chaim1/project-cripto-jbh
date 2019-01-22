a2850a8bb56629d80eff8fe847a09e8730ad32d3
chaim1 <cohen1ch@gmail.com>
2019-01-22 23:36:11 +0200

    update

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
+// window.onload = function () {
+//     jQuery('#homeClick').click();
+// }
 
-//https://api.coingecko.com/api/v3/coins/list
 var coinArray = [];
 
 $('#mainNav>li>a').click(function (e) {
+    $('#mainContent').empty();
+    $('#circle').show();
     e.preventDefault();
+    const link = $(this).attr('data-link');
     const href = $(this).attr('href');
     $.ajax(`templates/${href}.html`)
         .done(function (htmlData) {
-            if(coinArray.length == 0){
-                $.ajax({
-                    url: `demo.json`,
-                    method: 'GET'
-                }).done(function (d) {
-                    if (typeof d === 'string')
-                        d = JSON.parse(d);
-                        for(let i = 0; i < d.length; i++){
-                            coinArray.push(d[i]);
-                        }
-                        setData(coinArray, htmlData)
-                        console.log('coinArray');
-                });
-            }else{
-                setData(coinArray, htmlData)
-            }
+            getData(htmlData, link);
         });
 });
 
-function setData(coinArray, htmlData){
+const getData = (htmlData, link) => {
+    if (coinArray.length == 0) {
+        console.log(1);
+
+        $.ajax({
+            url: link,
+            method: 'GET',
+        }).done(function (d) {
+            if (typeof d === 'string')
+                d = JSON.parse(d);
+            for (let i = 0; i < d.length; i++) {
+                coinArray.push(d[i]);
+            }
+            setData(coinArray, htmlData)
+            console.log('coinArray');
+        });
+    } else {
+        console.log(2);
+        setData(coinArray, htmlData)
+    }
+}
+
+
+var precens = 3;
+function setData(coinArray, htmlData) {
     console.log(coinArray);
-    
+    var p = 0;
     for (let i = 0; i < coinArray.length; i++) {
-        // console.log(coinArray[i]['name']);
+        p++
+        var onePres = coinArray.length / 100;
+        if (p > onePres) {
+            precens++
+            p = 0;
+        }
+        if (precens > 99) {
+            $('#circle').hide();
+        }
         var temp = htmlData;
         temp = temp.replace('{simbol}', coinArray[i]['symbol']);
         temp = temp.replace('{name}', coinArray[i]['name']);
-        temp = temp.replace(/{collapse}/g, "cl"+coinArray[i]['id']);
+        temp = temp.replace(/{collapse}/g, "cl" + coinArray[i]['id']);
         $('#mainContent').append(temp);
     }
 }
@@ -51,28 +73,46 @@ var height = window.innerHeight;
 
 
 $window = $(window);
-var positionNav = $( "#header-nav" ).offset();
-var heightNav = $( "#header-nav" ).height();
+var positionNav = $("#header-nav").offset();
+var heightNav = $("#header-nav").height();
 $window.scroll(function (e) {
     let lastPosition = window.scrollY;
 
     if ($window.scrollTop() > positionNav.top) {
         console.log($window.scrollTop());
-        $( "#header-nav" ).addClass('fixed-top')
-        $( "#mainContent" ).css({marginTop: heightNav})
+        $("#header-nav").addClass('fixed-top')
+        $("#mainContent").css({ marginTop: heightNav })
     } else {
-        $( "#header-nav" ).removeClass('fixed-top')
-        $( "#mainContent" ).css({marginTop: '0'})
+        $("#header-nav").removeClass('fixed-top')
+        $("#mainContent").css({ marginTop: '0' })
     }
 });
 
 
 
-if(width<1100){
-    $( "#navBar" ).removeClass('container')
-    $( "#navBar" ).addClass('container-fluid')
-    $( "#divMenu" ).removeClass('container')
-    $( "#divMenu" ).addClass('container-fluid')
+if (width < 1100) {
+    $("#navBar").removeClass('container')
+    $("#navBar").addClass('container-fluid')
+    $("#divMenu").removeClass('container')
+    $("#divMenu").addClass('container-fluid')
 }
 
 
+function name(i) {
+ var interval =   setInterval(() => {
+        i++
+        var temp = `<div class="progress">
+    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="" aria-valuemin="0"
+        aria-valuemax="100" style="width:${ i }%">
+        ${ i }%
+    </div>
+    </div>`;
+    $('#test').html(temp);
+    console.log(precens);
+    if(i==100){
+        clearInterval(interval);
+    }
+    
+    }, 10);
+} 
+name(0)
